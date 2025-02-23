b91b5b2025e473327e72d9c39ec4d787ce38d933
Esteban Orduña <46265710+EstebanOrduna@users.noreply.github.com>
2025-02-23 14:00:23 -0600

    Rediseño profundo. Presentable

---
+++ loadProfile.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // document.getElementById('imagen-perfil'). = profile.imagen;
+    // Asignar el nombre de la foto de perfil
+    const fotoPerfil = document.querySelector(".profile-pic");
+    fotoPerfil.src = `assets/img/${profile.image}`;
+    
+    document.getElementById('nombre').innerText = profile.name;
+    document.getElementById('titulo').innerText = profile.title;
+    document.getElementById('experiencia').innerText = profile.experience;
+    
+    let serviciosLista = document.getElementById('lista-servicios');
+    profile.services.forEach(servicio => {
+        let li = document.createElement('li');
+        li.textContent = servicio;
+        serviciosLista.appendChild(li);
+    });
+
+    document.getElementById('email').innerText = profile.email;
+    document.getElementById('email').href = `mailto:${profile.email}`;
+    document.getElementById('whatsapp').innerText = 'Enviar mensaje';
+    document.getElementById('whatsapp').href = `https://wa.me/${profile.whatsapp}`;
+    document.getElementById('linkedin').innerText = 'Ver Perfil';
+    document.getElementById('linkedin').href = profile.linkedin;
+});
---
+++ profile.js
@@ -0,0 +1,11 @@
+const profile = {
+    "image": "bw.jpeg",
+    "name": "Esteban Orduña",
+    "title": "Ing. Mecatrónico | LSS Black Belt | MSc. Sistemas de Manufactura | MC. Tecnologías de la Información | QA Automation Engineer | SDET",
+    "intro": "Soy un apasionado de la tecnología y ecosistema emprendedor",
+    "experience": "Trabajando desde 2014 en temas relacionados a ingenieria de procesos, mejora continua, automatización y pruebas de software.",
+    "services": ["Automatización de procesos", "Consultoría de procesos", "Fotografía casual"],
+    "email": "esteban.orduna@gmail.com",
+    "whatsapp": "8123064812",
+    "linkedin": "https://www.linkedin.com/in/esteban-ordu%C3%B1a-045473104/"
+};
